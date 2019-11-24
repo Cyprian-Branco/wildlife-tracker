202f069f0e058b957e69a2d08e7c89b097cc0da2
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-24 20:52:18 +0300

    add database linking code

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.sql2o.Sql2o;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
 public class DB {
     private static URI dbUri;
@@ -12,9 +13,19 @@ public class DB {
     static Logger logger = LoggerFactory.getLogger(DB.class);
     static {
         try {
-            if(System.getenv("DATABASE_URL") == NULL){
-                dbUri = new URI("")
+            if(System.getenv("DATABASE_URL") == null){
+                dbUri = new URI("postgres://localhost:5432/wildlife_tracker");
+            }else{
+                dbUri = new URI(System.getenv("DATABASE_URL"));
             }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "postgres" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "Blackhole2123" : dbUri.getUserInfo().split(":")[1];
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e) {
+            logger.error("Unable to connect to the database");
         }
     }
 }
