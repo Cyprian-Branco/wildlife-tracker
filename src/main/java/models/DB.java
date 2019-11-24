b09598f8d3e45e8fbf5f9b2d7b8ac24788a9dea2
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-24 14:02:56 +0300

    connect to the postgres db

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -1,4 +1,20 @@
 package models;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sql2o.Sql2o;
+
+import java.net.URI;
+
 public class DB {
+    private static URI dbUri;
+    public static Sql2o sql2o;
+    static Logger logger = LoggerFactory.getLogger(DB.class);
+    static {
+        try {
+            if(System.getenv("DATABASE_URL") == NULL){
+                dbUri = new URI("")
+            }
+        }
+    }
 }
