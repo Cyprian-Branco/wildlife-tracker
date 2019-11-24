2ea037ded3b75b8e96836bb03849308de2cfeb25
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-24 21:59:26 +0300

    test getname method

--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -1,5 +1,20 @@
+package models;
+
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+    @Test
+    public void animal_instantiatesCorrectly_true(){
+        Animal animal = new Animal("Lion", "young");
+        assertTrue( animal instanceof Animal);
+    }
+    @Test
+    public void animal_nameInstantiatesCorrectly_String(){
+        Animal animal = new Animal ("Lion", "young");
+        assertEquals("Lion", animal.getName());
 
-}
+    }
+g}
