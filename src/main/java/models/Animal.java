c30056a220cd1014cd4051e5375b507fb12efd46
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-24 21:57:48 +0300

    add animal variables

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,4 +1,17 @@
 package models;
 
 public class Animal {
+    public String name;
+    public String age;
+    public String type;
+    public int id;
+    public static final String[] ageArray = {"newborn", "young", "adult"};
+    public Animal (String name, String age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
