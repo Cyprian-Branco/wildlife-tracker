cc380100697a7fa8dd3e54cbb48ac7968920c081
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-25 09:49:17 +0300

    test if ranger instantiates correctly

--- src/test/java/models/SightingTest.java
+++ src/test/java/models/SightingTest.java
@@ -1,5 +1,21 @@
+package models;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SightingTest {
+    @Test
+    public void sighting_instantiatesCorrectly_True(){
+        Sighting sighting = new Sighting("Mike", 2, "Tsavo");
+        assertTrue(sighting instanceof Sighting);
+    }
+    @Test
+    public void rangerInstantiatesCorrectly_String(){
+        Sighting sighting = new Sighting("Mike", 2, "Tsavo");
+        assertEquals("Mike", sighting.getRanger());
+    }
+
+    
 
 }
