17c86c995c53a14b4e74a491b17d3752c494fb16
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-25 20:01:14 +0300

    initialize variables

---
+++ src/main/java/models/Endangered.java
@@ -0,0 +1,27 @@
+package models;
+
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class Endangered  extends Animal{
+    public String health;
+    public static final String DATABASE_TYPE = "endangered";
+
+    public Endangered(String name, String age, String health){
+        super();
+        this.name = name;
+        this.age = age;
+        this.health = health;
+        type = DATABASE_TYPE;
+    }
+    public String getHealth(){
+        return health;
+    }
+    public static List<Endangered> all(){
+        String sql = "SELECT * FROM WHERE type='endangered';";
+        try (Connection con = DB.sql2o.open()){
+            return con.createQuery(sql).throwOnMappingFailure(false).executeAndFetch(Endangered.class);
+        }
+    }
+}
