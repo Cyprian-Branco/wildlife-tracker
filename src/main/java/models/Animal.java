1fa4368c0cf2ef7be5d2fe98cb6536b58ddc40e8
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-25 08:06:07 +0300

    add save and find methods

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,5 +1,10 @@
 package models;
 
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
+import java.util.Objects;
+
 public class Animal {
     public String name;
     public String age;
@@ -17,5 +22,35 @@ public class Animal {
 
     public String getAge() {
         return age;
+    }
+    @Override
+    public boolean equals (Object o ){
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(getName(), animal.getName());
+    }
+    public int hashcode(){
+        return Objects.hash(getName());
+    }
+    public void save(){
+        try (Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO animals (name, age, type) VALUES (:name, :age, :type)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("age", this.age)
+                    .addParameter("type", this.type)
+                    .executeUpdate()
+                    .getKey();
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+    public static Animal find(int id) {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM animals where id = :id;";
+            Animal animal = con.createQuery(sql).addParameter("id", id).throwOnMappingFailure(false).executeAndFetchFirst(Animal.class);
+            return animal;
+        }
     }
 }
