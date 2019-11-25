a70df8ce0a4dad08e33a4be283a1394aa67975ce
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-25 20:21:24 +0300

    test if endangered class instantioates correctly

--- src/test/java/models/EndangeredTest.java
+++ src/test/java/models/EndangeredTest.java
@@ -1,5 +1,17 @@
+package models;
+
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EndangeredTest {
+
+    @Test
+    public void endangeredAnimal_InstantiatesCorrectly_true() {
+        Endangered testEndangered = new Endangered("Rhino", "healthy", "young");
+        assertTrue(testEndangered instanceof Endangered);
+    }
+    
 
 }
